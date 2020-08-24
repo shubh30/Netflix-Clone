4e362d3e42053b513d1022b5c5d366586bd5a265
Shubham <shubhamnagpal30@gmail.com>
2020-08-24 12:01:04 +0530

    Render dome more rows

--- src/Row.js
+++ src/Row.js
@@ -7,9 +7,7 @@ const base_url = 'https://image.tmdb.org/t/p/original/';
 function Row({ title, fetchUrl }) {
   const [movies, setMovies] = useState([]);
 
-  // A snippet of code based on specific condition/variable
   useEffect(() => {
-    // if [], run once when the row loads, and then don't run it again
     async function fetchData() {
       // It will append the unique end point url to the base url ("https://api.themoviedb.org/3" + '/discover/tv?api_key=${API_KEY}&with_networks=213')
       const request = await axios.get(fetchUrl);
