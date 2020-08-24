8fc49ebd39109073612af9cd66c84dd8e7716424
Shubham <shubhamnagpal30@gmail.com>
2020-08-24 16:06:36 +0530

    Render banner with styles

--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from './axios';
 import requests from './requests';
+import './Banner.css';
 
 function Banner(props) {
   const [movie, setMovie] = useState([]);
@@ -20,24 +21,32 @@ function Banner(props) {
 
   console.log(movie);
 
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + '...' : str;
+  }
+
   return (
-    <header className='banner'
+    <header
+      className="banner"
       style={{
-        backgroundSize: "cover",
+        backgroundSize: 'cover',
         backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
-        backgroundPosition: "center center"
+        backgroundPosition: 'center center',
       }}
     >
-      <div className='banner__contents'>
-      <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
-      <div className='banner__buttons'>
-        <button className='banner__button'>Play</button>
-        <button className='banner__button'>My List</button>
+      <div className="banner__contents">
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+        <div className="banner__buttons">
+          <button className="banner__button">Play</button>
+          <button className="banner__button">My List</button>
+        </div>
+        <h1 className="banner__description">
+          {truncate(movie?.overview, 150)}
+        </h1>
       </div>
-      <h1 className='banner__description'>
-        {movie?.overview}
-      </h1>
-      </div>
+      <div className='banner__fadeBottom'></div>
     </header>
   );
 }
