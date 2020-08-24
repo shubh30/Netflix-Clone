28bda3405d640f9c8d67d41b42d165be68dfe43f
Shubham <shubhamnagpal30@gmail.com>
2020-08-24 11:46:56 +0530

    Add some styles to each row

--- src/Row.js
+++ src/Row.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from './axios';
+import './Row.css';
 
 const base_url = 'https://image.tmdb.org/t/p/original/';
 
@@ -25,10 +26,17 @@ function Row({ title, fetchUrl }) {
       <h2>{title}</h2>
 
       {/* container -> posters */}
-      <div className="">
+      <div className="row__posters">
         {/* Posters */}
         {movies.map((movie) => {
-          return <img src={`${base_url}${movie.poster_path}`} alt={movie.name} />;
+          return (
+            <img
+              key={movie.id}
+              className="row__poster"
+              src={`${base_url}${movie.poster_path}`}
+              alt={movie.name}
+            />
+          );
         })}
       </div>
 
