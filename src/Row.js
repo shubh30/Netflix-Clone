b20cd21f069206fb2bffe3397cd8c82648ac283c
Shubham <shubhamnagpal30@gmail.com>
2020-08-24 12:20:57 +0530

    Differentiate rows using isLargeRow props

--- src/Row.js
+++ src/Row.js
@@ -4,7 +4,7 @@ import './Row.css';
 
 const base_url = 'https://image.tmdb.org/t/p/original/';
 
-function Row({ title, fetchUrl }) {
+function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
@@ -30,8 +30,8 @@ function Row({ title, fetchUrl }) {
           return (
             <img
               key={movie.id}
-              className="row__poster"
-              src={`${base_url}${movie.poster_path}`}
+              className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+              src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
               alt={movie.name}
             />
           );
